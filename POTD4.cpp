79c733f483164dfa568f743b61e815009c4ffdaa
Atharv Powalkar <87745693+07-Atharv@users.noreply.github.com>
2023-04-10 06:03:10 +0530

    Add files via upload

---
+++ POTD4.cpp
@@ -0,0 +1,5 @@
+// The problem is about we have given the specific position x and y at that position 1 is assigned and we have to fill the  2d matrix in minimum iterations
+// So basically we are going to use the BFS approach for the whole matrix to make the positions 1 with min iterations and return the iteration
+// we are going to use the queue data structure and then we are going to store the coordinates next we are going to use the booleab=n array to keep the track of the visited nodes
+// Trverse the queue till the queue is not empty and till the end of the queue
+// take an integer array which is going to store the current coordinate from the queue
