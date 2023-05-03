b8b08df50ca87c12932bba505e071b39eec37fcb
Atharv Powalkar <87745693+07-Atharv@users.noreply.github.com>
2023-05-03 06:41:41 +0530

    Add files via upload

---
+++ gfg16.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 1004, r;
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     r = n % 10;
+    // }
+    // cout << r;
+    // r++;
+    while (n > 0)
+    {
+        r = n % 10;
+    }
+    cout << r << endl;
+}
